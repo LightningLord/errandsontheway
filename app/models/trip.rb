c17f30c6c97953a8290e6e3ed5d48e8386332490
Melanie Archer <job@twobanjos.com>
2014-04-10 18:29:57 -0700

    Rename `Route` to `Trip`
    * stop busting RSpec with Rails keyword

---
+++ app/models/trip.rb
@@ -0,0 +1,8 @@
+class Trip < ActiveRecord::Base
+  has_many :errands
+  validates :start_point_longitude,
+            :start_point_latitude,
+            :end_point_longitude,
+            :end_point_latitude,
+            presence: true
+end
