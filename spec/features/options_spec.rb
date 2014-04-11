51be14db9c43e4d4bf943c52b8674decb085451b
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-10 18:23:55 -0700

    OptionsSpec: Use Faker-generated route_path

--- spec/features/options_spec.rb
+++ spec/features/options_spec.rb
@@ -3,7 +3,7 @@ feature "Options", :js => false do
   describe"return a list of business options" do
     let(:my_route){FactoryGirl.create(:route)}
     it "returns three business options" do
-      visit route_path, :id => my_route.id
+      visit route_path(my_route)
       fill_in "search", :with => "Laundry"
       click_on "Search"
       expect(page).to have_content "Search Results"
