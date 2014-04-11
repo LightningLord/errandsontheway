6de28d97a7c4c36eb56fa80c24cb0c4f75846abc
Ryan Wood <rbn1357@gmail.com>
2014-04-11 15:39:58 -0700

    added test for create for failing params

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -12,9 +12,14 @@ describe TripsController do
     end
   end
   describe "#create" do
-    it "should create a new trip" do
+    it "should create a new trip with valid addresses" do
       expect{post :create, trip: FactoryGirl.attributes_for(:trip) }.to change {Trip.count}.by(1)
       expect(response).to be_redirect
+    end
+
+    it "should render new page for invalid addresses" do
+      post :create, trip: {"start_point_address"=>"1BadAddress", "end_point_address"=>"2Bad Address"}
+      expect(response).to render_template(:new) 
     end
   end
 end
