69f9d91f7b9b19ba7d495b44364e70e2cf47abed
Christine Dolendo <chris.dolendo@gmail.com>
2014-04-10 20:21:30 -0700

    added trips#new tests

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe TripsController do
+  describe "#new" do
+    it "renders new page" do
+      get :new
+      expect(response).to render_template(:new)
+    end
 
+    it "should assign a trip variable" do
+      get :new
+      expect(assigns(:trip)).to be_a_new(Trip)
+    end
+  end
 end
