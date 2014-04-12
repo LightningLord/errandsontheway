9331cf134dd3851b7b0d3738e784c3a2e6ad7251
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-12 14:58:44 -0700

    Errand needs its own coordinates model

--- app/models/errand.rb
+++ app/models/errand.rb
@@ -2,5 +2,8 @@ class Errand < ActiveRecord::Base
   belongs_to :trip
   validates :business_name, :latitude, :longitude, :additional_duration, presence: true
 
-
+  def set_coordinates(coords)
+    self.latitude = coords.first
+    self.longitude = coords.last
+  end
 end
