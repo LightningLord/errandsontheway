b6fdccdea0d83607439f37f20498120e2c50deca
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-12 14:34:23 -0700

    refactored getting coordinates to accept only one address. Trello #54

--- app/models/coordinates_retriever.rb
+++ app/models/coordinates_retriever.rb
@@ -1,12 +1,7 @@
-class CoordinatesRetriever 
+class CoordinatesRetriever
 
-  def self.get_coordinates(start_address, end_address)
-    start_result = Geocoder.coordinates(start_address)
-    end_result = Geocoder.coordinates(end_address)
-    if start_result.nil? || end_result.nil?
-      nil
-    else
-      [start_result, end_result]
-    end
+  def self.get_coordinates(address)
+    result = Geocoder.coordinates(address)
+    result.nil? ? nil : result
   end
 end
