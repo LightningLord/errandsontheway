d074f0fbbb9a3a47fc2229a82200c505e541af69
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-12 15:54:43 -0700

    two trip model specs passing

--- spec/features/options_spec.rb
+++ spec/features/options_spec.rb
@@ -3,20 +3,18 @@ feature "Options", :js => false do
   describe"return a list of business options" do
     let(:my_trip){FactoryGirl.create(:valid_trip)}
 
-    before(:each){
+    before(:each) do
       visit root_path
       fill_in "start-point", with: "460 Fell St. San Francisco, CA"
       fill_in "end-point", with: "633 Folsom St. San Francisco, CA"
       click_on "Route my trip!"
       fill_in "search", :with => "Laundry"
       click_on "Search"
-    }
+    end
     it "displays the search term" do
       expect(page).to have_content "Laundry"
     end
 
-    xit "returns three business related to search term" do
-      expect(page).to have_content ""
-    end
+
   end
 end
