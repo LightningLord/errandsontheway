8d778a9cac1fc34698b0c62934a98f71808faf73
Melanie Archer & Ryan <pair+mejarc.rbn1357@devbootcamp.com>
2014-04-12 16:08:20 -0700

    Calculate extra time in minutes (trello 55)
      * adding new helper for converting seconds to minutes for views
      * removing formatting chore from Business model
      * each Option will only return its extra duration in seconds

--- app/models/business.rb
+++ app/models/business.rb
@@ -11,9 +11,4 @@ class Business
     duration_with_business - original_trip_duration
   end
 
-  def format_delta_duration(duration_with_business, original_trip_duration)
-    "#{calculate_delta_duration(duration_with_business, original_trip_duration) / 60 } minutes"
-
-  end
-
 end
