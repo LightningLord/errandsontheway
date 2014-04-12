4319807d5866a6c83c079df7ce4820e69daa39fa
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-12 12:14:04 -0700

    Obtaining places near search location
    * still need to work out testing

--- spec/controllers/options_controller_spec.rb
+++ spec/controllers/options_controller_spec.rb
@@ -3,11 +3,24 @@ require 'spec_helper'
 describe OptionsController do
   describe '#index' do
     let(:new_trip){FactoryGirl.create(:valid_trip)}
-    it 'assigns a search term based on params' do
+    let(:business){{name: "Laundromat", address: "717 California Street, San Francisco"}}
+
+    before(:each) do
       request.session[:trip_id] = new_trip.id
       get :index, :search => "Tacos"
+    end
+
+    it 'assigns a search term based on params' do
       expect(assigns(:search_term)).to eq('Tacos')
     end
 
+    # it 'assigns @businesses to four businesses' do
+    #   Place.any_instance.stub(:request_businesses).and_return([{"name" => business[:name], "vicinity" => business[:address]}])
+    #   stub_return = {"routes" => [{"legs" => [{"duration" => {"value" => 50} }] }]}
+    #   DirectionsServiceHelper.any_instance.stub(:api_request).and_return(stub_return)
+    #   p assigns(:businesses)
+    # end
+
   end
 end
+
