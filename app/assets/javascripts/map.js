15a76afa7d33255fc49f76fd32b8a8c62a9c7ff7
Christine Dolendo <chris.dolendo@gmail.com>
2014-04-13 16:26:33 -0700

    implemented toggle for map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -74,3 +74,35 @@ function geolocate() {
   }
 }
 
+var activateDirectionsToggle = function(){
+  var directionsPanel = document.getElementById('directions-panel');
+  directionsPanel.style.display = 'none';
+  var showDirectionsLink = document.getElementById('directions');
+
+  showDirectionsLink.addEventListener('click', function(event){
+    event.preventDefault();
+    $("#map-canvas").slideToggle("drop");
+    $("#directions-panel").slideToggle("drop");
+    updateDirectionsLink();
+  });
+
+};
+
+function updateDirectionsLink(){
+ 
+  if ($("#directions").text() === "Show Directions"){
+    $("#directions").text("Hide Directions");
+  }
+
+  else if ($("#directions").text() === "Hide Directions"){
+    $("#directions").text("Show Directions");
+  }
+
+}
+
+
+
+
+
+
+
