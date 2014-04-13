736161833466a5e32cb3bc8a48be450af77e20cb
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-12 20:36:49 -0700

    waypoints typo

--- app/models/directions_service_helper.rb
+++ app/models/directions_service_helper.rb
@@ -11,7 +11,7 @@ class DirectionsServiceHelper
   def initialize(args)
       @options = { query: { origin: args[:origin],
                             destination: args[:destination],
-                            waypoints: "optimize:true" + args[:waypoints].join(" | "),
+                            waypoints: "optimize:true|" + args[:waypoints].join(" | "),
                             sensor: false,
 
                             key: ENV['API_KEY']
