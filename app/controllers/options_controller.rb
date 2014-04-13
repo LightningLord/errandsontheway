851beae8c3ff10acd3a7cfd71dcc36c6c9df1a7c
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-13 14:34:49 -0700

    check if delta is negative, if so change to 0

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -18,6 +18,7 @@ class OptionsController < ApplicationController
       waypoints_list += trip.errands.map{|errand| errand.address} unless trip.errands.empty?
       trip_info = {origin: trip.start_point_address, destination: trip.end_point_address, waypoints: waypoints_list}
       business.extra_duration = business.calculate_delta_duration(DirectionsServiceHelper.new(trip_info).calculate_total_duration, trip.original_duration)
+      business.extra_duration = 0 if business.extra_duration < 0
     end
     return businesses
   end
