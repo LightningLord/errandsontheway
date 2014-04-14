a138db4c1d4089c4123b167b396226996e2cc762
Strand McCutchen <strand@bettermistak.es>
2014-04-14 16:57:52 -0700

    Reviewed the Distance Matrix Helper

--- app/models/distance_matrix_helper.rb
+++ app/models/distance_matrix_helper.rb
@@ -11,6 +11,9 @@ class DistanceMatrixHelper
                           key: ENV["API_KEY"] } }
   end
 
+  # This seems to be doing the same thing or a similar thing as
+  # DirectionsServiceHelper#calculate_total_duration. Is there a missing object
+  # here?
   def get_trip_duration
     make_api_call["rows"].first["elements"].first["duration"]["value"]
   end
