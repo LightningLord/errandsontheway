a1779011ad4ed663014bd51094d58deb16c6f7e3
Strand McCutchen <strand@bettermistak.es>
2014-04-14 16:42:46 -0700

    Reviewed ShortenedLink

--- app/models/shortened_link.rb
+++ app/models/shortened_link.rb
@@ -1,6 +1,13 @@
 class ShortenedLink
+
+  # I would make charset a private method. This is nice, but I think that I
+  # would just do something like: `SecureRandom.urlsafe_base64[0...size]`
   def self.generate_random_string(size = 6)
-    charset = ('a'..'z').to_a + (0..9).to_a
     (0...size).map{ charset.to_a[rand(charset.size)] }.join
+  end
+
+  private
+  def charset
+    ('a'..'z').to_a + (0..9).to_a
   end
 end
