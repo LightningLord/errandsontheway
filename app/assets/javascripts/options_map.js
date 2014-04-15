99cb4924e4ef2b0de8079b3afba50e18e0534e05
Christine Dolendo <chris.dolendo@gmail.com>
2014-04-15 11:56:38 -0700

    rendered map on options page showing current route. options points not yet implemented:

--- app/assets/javascripts/options_map.js
+++ app/assets/javascripts/options_map.js
@@ -1,4 +1,33 @@
 var renderOptionsMap = function(trip, options){
-  
+  var directionsDisplay;
+  var directionsService = new google.maps.DirectionsService();
+  var map;
 
+  function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    map = new google.maps.Map(document.getElementById('map-canvas'));
+    directionsDisplay.setMap(map);
+    calcRoute();
+  }
+
+  function calcRoute() {
+    var request;    
+    request = {
+        origin: trip.start_point_address,
+        destination: trip.end_point_address,
+        travelMode: google.maps.TravelMode[trip.travel_mode.toUpperCase()]
+    };
+
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      }
+    });
+
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.trigger(window, 'load');
+  
 };
+
--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -5,8 +5,9 @@ class OptionsController < ApplicationController
       @trip = Trip.find(session[:trip_id])
       start_place = Place.new(@trip.start_point_latitude.to_s, @trip.start_point_longitude.to_s, @search_term)
       end_place = Place.new(@trip.end_point_latitude.to_s, @trip.end_point_longitude.to_s, @search_term)
-      @start_businesses = get_business_info_near_point(start_place, @trip)
-      @end_businesses = get_business_info_near_point(end_place, @trip)
+      p @start_businesses = get_business_info_near_point(start_place, @trip)
+      p @end_businesses = get_business_info_near_point(end_place, @trip)
+      p @options = @start_businesses + @end_businesses
       render :partial => "options"
     else
       redirect_to root_path
