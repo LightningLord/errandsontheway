52eb27ae2af1529434121af88c1fa79dadefc6d1
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-15 08:34:21 -0700

    Fixed tests to match new styling changes

--- spec/features/trips_spec.rb
+++ spec/features/trips_spec.rb
@@ -4,14 +4,12 @@ feature "Trips", :js => true do
   describe "Trips" do
     context "with valid form fields" do
       before(:each) do
-        VCR.use_cassette('trip_spec') do
+        VCR.use_cassette('trips_spec') do
           visit root_path
           fill_in "Where are you starting?", with: "460 Fell St. San Francisco, CA"
           fill_in "Where are you going?", with: "633 Folsom St. San Francisco, CA"
           find(:css, "#walking").click
-          wait_for_ajax_to_finish
           click_on "Route my trip!"
-          save_and_open_page
         end
       end
       it "successfully submits your origin and destination" do
@@ -20,7 +18,7 @@ feature "Trips", :js => true do
 
       it "finalizes a trip" do
         click_link "Finalize Trip"
-        expect(page).to have_content("Trip Duration With Errands")
+        expect(page).to have_content("With Errands")
       end
 
       it "starts over" do
