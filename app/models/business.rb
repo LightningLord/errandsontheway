3d67dba1a227e2fbcfccfcaa9f98a2392f02013f
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-15 15:13:49 -0700

    refactored: extraxted new duration api call

--- app/models/business.rb
+++ app/models/business.rb
@@ -10,10 +10,15 @@ class Business
 
 
   def set_directions(trip)
-    set_extra_duration(DirectionsServiceHelper.new(trip.trip_info(@address)).calculate_total_duration, trip.original_duration)
+    set_extra_duration(get_new_trip_duration(trip), trip.original_duration)
   end
 
   private
+
+  def get_new_trip_duration(trip)
+    DirectionsServiceHelper.new(trip.trip_info(@address)).calculate_total_duration
+  end
+
   def set_extra_duration(duration_with_business, original_trip_duration)
     @extra_duration = duration_with_business - original_trip_duration
     @extra_duration = 0 if @extra_duration < 0
