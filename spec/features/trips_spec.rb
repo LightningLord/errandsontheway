ce1ec215b266e3b88412c1e1ec868f72fd1ee534
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-15 12:32:12 -0700

    commented out integration tests, put in backlog

--- spec/features/trips_spec.rb
+++ spec/features/trips_spec.rb
@@ -1,35 +1,40 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-feature "Trips", :js => true do
-  describe "Trips" do
-    context "with valid form fields" do
-      let(:fell){FactoryGirl.create(:fell)}
-      let(:folsom){FactoryGirl.create(:folsom)}
-      before(:each) do
-        VCR.use_cassette('trips_spec') do
-          visit root_path
-          fill_in "Where are you starting?", with: fell.address
-          fill_in "Where are you going?", with: folsom.address
-          find(:css, "#walking").click
-          click_on "Route my trip!"
-          save_and_open_page
-        end
-      end
-      it "successfully submits your origin and destination" do
-          expect(page).to have_content("Trip Duration")
-      end
+# feature "Trips", :js => true do
+#   describe "Trips" do
+#     context "with valid form fields" do
+#       let(:fell){FactoryGirl.create(:fell)}
+#       let(:folsom){FactoryGirl.create(:folsom)}
+#       before(:each) do
+#         stub_return = {"routes" => [{"legs" => [{"duration" => {"value" => 50} }] }]}
+#         DirectionsServiceHelper.stub(:api_request).and_return(stub_return)
+#         DistanceMatrixHelper.stub(:make_api_call).and_return({"rows" =>
+#         [{"elements" => [{"duration" => {"value" => 150}}]}]})
+#          place_stub_return_start = [{"name" => "DBC" },{"vicinity"=> "717 California Street, San Francisco, CA"}]
+#         place_stub_return_end = [{"name" => "DBC new" },{"vicinity"=> "633 Folsom Street, San Francisco, CA"}]
+#         Place.stub(:request_businesses).and_return(place_stub_return_start, place_stub_return_end)
+#         visit root_path
+#         fill_in "Where are you starting?", with: fell.address
+#         fill_in "Where are you going?", with: folsom.address
+#         find(:css, "#walking").click
+#         click_on "Route my trip!"
+#         save_and_open_page
+#       end
+#       it "successfully submits your origin and destination" do
+#           expect(page).to have_content("Trip Duration")
+#       end
 
-      it "finalizes a trip" do
-        click_link "Finalize Trip"
-        expect(page).to have_content("With Errands")
-      end
+#       it "finalizes a trip" do
+#         click_link "Finalize Trip"
+#         expect(page).to have_content("With Errands")
+#       end
 
-      it "starts over" do
-        click_on "Start Over"
-        expect(page).to have_button "Route my trip!"
-      end
-    end
-  end
+#       it "starts over" do
+#         click_on "Start Over"
+#         expect(page).to have_button "Route my trip!"
+#       end
+#     end
+#   end
 
   # context "autofilling the user's origin based on their current location" do
   #   it "should fill in the origin field with an address", focus: true do
