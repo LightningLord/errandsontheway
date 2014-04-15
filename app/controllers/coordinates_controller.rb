7182ab0f56380dca2f4088e296708d87a7db417c
Ryan Wood <rbn1357@gmail.com>
2014-04-14 18:53:25 -0700

    refactored to remove hash rocket and comply with newer syntax

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -1,6 +1,6 @@
 class CoordinatesController < ApplicationController
   def convert
     results = CoordinatesRetriever.get_address(params["coords"])
-    render :text => "#{results}"
+    render text: "#{results}"
   end
 end 
