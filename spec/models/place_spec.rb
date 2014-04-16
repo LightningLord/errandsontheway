77ba3dcbaac365b9ec7ff18117ec105d44a94ac5
Jason Lo <mr.lo.mhs@gmail.com>
2014-04-15 17:19:33 -0700

    refactored place_spec.rb

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -3,8 +3,15 @@ require 'spec_helper'
 describe Place do
   let(:place){ Place.new('-33.87054', '151.198815', "laundry")}
   let(:business){{name: "Laundromat", address: "717 California Street, San Francisco", latitude: 37.7, longitude: -122}}
+
   before(:each) do
-    Place.stub(:get_biz_names_addresses_coordinates).and_return([{"name" => business[:name], "vicinity" => business[:address], "latitude"=> business[:latitude], "longitude"=>business[:longitude]}])
+    place_stub_return = [{"name" => "DBC",
+    "vicinity"=> "717 California Street, San Francisco, CA",
+    "geometry" =>{"location" => {"lat" =>37.7, "lng" =>  -122 }} },
+    {"name" => "DBC",
+    "vicinity"=> "717 California Street, San Francisco, CA",
+    "geometry" =>{"location" => {"lat" =>37.7, "lng" =>  -122 }} }]
+    Place.stub(:request_businesses).and_return(place_stub_return)
   end
 
   it "extracts each business's name address, and coordinates" do
